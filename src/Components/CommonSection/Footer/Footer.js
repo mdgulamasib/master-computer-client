4098c2f393c897ed02291f08062973e177dbae0f
mdgulamasib <gulam.asib@gmail.com>
2022-04-29 07:28:07 +0600

    basic banner added

--- src/Components/CommonSection/Footer/Footer.js
+++ src/Components/CommonSection/Footer/Footer.js
@@ -10,67 +10,66 @@ const Footer = () => {
     const year = today.getFullYear();
 
     return (
-        <>
-            <footer className="text-white text-center text-lg-start bg-dark">
 
-                <div className="container p-4">
+        <footer className="text-white text-center text-lg-start bg-dark">
 
-                    <div className="row mt-4">
+            <div className="container p-4">
 
-                        <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
-                            <h5 className="text-uppercase mb-4">Master Computer</h5>
-                            <span className="fa-li"><i className="fas fa-home mb-3"></i></span><span  >Sheikhergaon, Ramsree-3301, Habiganj</span><br />
-                            <span className="fa-li"><i className="fas fa-envelope"></i></span><span  >contact@mastercomputer.com</span><br />
-                            <span className="fa-li"><i className="fas fa-phone"></i></span><span  >+880 1770000000</span>
-                        </div>
+                <div className="row mt-4">
 
-
-
-                        <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
-                            <h5 className="text-uppercase mb-4 pb-1">Social Links</h5>
-                            <p>Visit our official social profiles</p>
-
-                            <a href="http://www.facebook.com" className='link-style me-2' target='_blank' rel="noopener noreferrer"> <BsFacebook className='fs-5' /></a>
-                            <a href="http://www.instagram.com" className='link-style me-2' target='_blank' rel="noopener noreferrer"> <BsInstagram className='fs-5' /></a>
-                            <a href="http://www.linkedin.com" className='link-style' target='_blank' rel="noopener noreferrer"> <BsLinkedin className='fs-5' /></a>
+                    <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
+                        <h5 className="text-uppercase mb-4">Master Computer</h5>
+                        <span className="fa-li"><i className="fas fa-home mb-3"></i></span><span  >Sheikhergaon, Ramsree-3301, Habiganj</span><br />
+                        <span className="fa-li"><i className="fas fa-envelope"></i></span><span  >contact@mastercomputer.com</span><br />
+                        <span className="fa-li"><i className="fas fa-phone"></i></span><span  >+880 1770000000</span>
+                    </div>
 
 
-                        </div>
 
-                        <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
-                            <h5 className="text-uppercase mb-4">Opening hours</h5>
+                    <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+                        <h5 className="text-uppercase mb-4 pb-1">Social Links</h5>
+                        <p>Visit our official social profiles</p>
 
-                            <table className="table text-center text-white">
-                                <tbody className="fw-normal">
-                                    <tr>
-                                        <td>Mon - Thu:</td>
-                                        <td>8am - 9pm</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Fri - Sat:</td>
-                                        <td>8am - 1am</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Sunday:</td>
-                                        <td>9am - 10pm</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                        <a href="http://www.facebook.com" className='link-style me-2' target='_blank' rel="noopener noreferrer"> <BsFacebook className='fs-5' /></a>
+                        <a href="http://www.instagram.com" className='link-style me-2' target='_blank' rel="noopener noreferrer"> <BsInstagram className='fs-5' /></a>
+                        <a href="http://www.linkedin.com" className='link-style' target='_blank' rel="noopener noreferrer"> <BsLinkedin className='fs-5' /></a>
 
+
+                    </div>
+
+                    <div className="col-lg-4 col-md-6 mb-0 mb-md-0">
+                        <h5 className="text-uppercase mb-4">Opening hours</h5>
+
+                        <table className="table text-center text-white">
+                            <tbody className="fw-normal">
+                                <tr>
+                                    <td>Mon - Thu:</td>
+                                    <td>8am - 9pm</td>
+                                </tr>
+                                <tr>
+                                    <td>Fri - Sat:</td>
+                                    <td>8am - 1am</td>
+                                </tr>
+                                <tr>
+                                    <td>Sunday:</td>
+                                    <td>9am - 10pm</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
 
                 </div>
 
+            </div>
 
 
-                <div className="text-center p-3">
-                    <p><small>copyright {copy} {year} <a className='link-style' href="#">mastercomputer.com</a><span> Developed by <a className='link-style' href="#">mdgulamasib.com</a> </span> </small> </p>
-                </div>
+
+            <div className="text-center p-3">
+                <p><small>copyright {copy} {year} <a className='link-style' href="#">mastercomputer.com</a><span> Developed by <a className='link-style' href="#">mdgulamasib.com</a> </span> </small> </p>
+            </div>
 
 
-            </footer >
-        </>
+        </footer >
     );
 };
 
--- src/Components/Pages/Home/Banner/Banner.js
+++ src/Components/Pages/Home/Banner/Banner.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import './Banner.css';
+import { Carousel, CarouselItem, NavItem } from 'react-bootstrap';
+import img1 from '../../../../images/banner/img1.png'
+import img2 from '../../../../images/banner/img2.png'
+import img3 from '../../../../images/banner/img3.png'
 
 const Banner = () => {
+
     return (
-        <div>
-            <h2>This is banner</h2>
+        <div className='background-img'>
+            <div className='container w-75'>
+                <div className='text-center'>
+                    <h3 className='pt-5'>Welcome to</h3>
+                    <h3>Master Computer</h3>
+                    <h3 className='mb-0 pb-5'> Warehouse Management System</h3>
+                </div>
+                <div className='highlighting pb-5'>
+                    <div>
+                        <img className='img-size' src={img1} alt="" />
+                        <h5>User Friendly Software</h5>
+                    </div>
+                    <div>
+                        <img className='img-size' src={img2} alt="" />
+                        <h5>Easy Warehouse Management</h5>
+                    </div>
+                    <div>
+                        <img className='img-size' src={img3} alt="" />
+                        <h5>Hassle Free Navigation</h5>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
